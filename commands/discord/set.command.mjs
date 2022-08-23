d7765c47f3687f29808705fb2eddc3c46c154722
OmegaPirat <co9516514@gmail.com>
2022-08-23 11:54:19 +0200

    Prevent making a channel subscripable if it is already subscripable

--- commands/discord/set.command.mjs
+++ commands/discord/set.command.mjs
@@ -4,11 +4,27 @@ import { discordBot } from '../../bots/index.mjs';
 
 let set = {
     execute: async interaction => {
-        await db.DiscordChannel.create({
-            channelId: interaction.channelId,
-            channelName: discordBot.channels.cache.get(interaction.channelId).name,
-            subscriptionIdentifier: interaction.options.getString("id") ?? discordBot.channels.cache.get(interaction.channelId).name
+        let channelId = interaction.channelId;
+        let channelName = discordBot.channels.cache.get(interaction.channelId).name;
+        let subscriptionIdentifier = interaction.options.getString("id") ?? discordBot.channels.cache.get(interaction.channelId).name;
+
+        let subscribedChannel = await db.DiscordChannel.findOne({
+            where: {
+                channelId
+            }
         });
+
+        if (subscribedChannel) {
+            console.log("Dieser Kanal ist bereits abonnierbar!");
+            return;
+        }
+
+        await db.DiscordChannel.create({
+            channelId,
+            channelName,
+            subscriptionIdentifier
+        });
+
         await interaction.reply({
             content: "Dieser Kanal ist nun abonnierbar!",
             ephemeral: true
