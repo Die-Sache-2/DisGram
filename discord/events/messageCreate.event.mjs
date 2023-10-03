2d7982deb17b840f2f74438cd4f38eaf47744cc6
Bernd Siemer <Schattenpirat>
2023-10-03 12:58:13 +0200

    Test rabbit mq eschange

--- discord/events/messageCreate.event.mjs
+++ discord/events/messageCreate.event.mjs
@@ -1,48 +1,22 @@
 import db from '../../db/index.mjs';
-import telegramBot from '../../telegram/index.mjs';
-import {setLongTimeout} from "long-settimeout";
 import {markdownv2 as format} from "telegram-format";
 import rabbitmq from "../../rabbitmq/index.mjs";
 
+let ch = await rabbitmq.createChannel();
+for(let i = 0; i<0; i++)
+setInterval(
+    () => {
+        ch.sendToQueue("task", Buffer.from("Hallo"));
+    }, 0
+)
+
 let messageCreate = async message => {
-    let subscriptions = await db.Subscription.findAll({
-        include: [
-            {
-                model: db.DiscordChannel,
-                attributes: ["channelId"]
-            },
-            {
-                model: db.TelegramChannel,
-                attributes: ["channelId"]
-            },
-        ]
-    })
-
-    let parsedSubscriptions = subscriptions.map(it => it.dataValues).map(it => ({
-        id: it.id,
-        discordChannelId: it.DiscordChannel.dataValues.channelId,
-        telegramChannelId: it.TelegramChannel.dataValues.channelId,
-        retentionTime: it.retentionTime
-    }));
-
-    for (let subscription of parsedSubscriptions) {
-        let {telegramChannelId, discordChannelId, retentionTime} = subscription;
-        if (message.channelId === discordChannelId) {
-            let channel = await rabbitmq.createChannel();
-            channel.sendToQueue("task", Buffer.from(await appendSignature(format.escape(message.content))));
-            let telegramMessage = await telegramBot.telegram.sendMessage(telegramChannelId, await appendSignature(format.escape(message.content)), {parse_mode: 'MarkdownV2'});
-            await db.MessageLink.create({
-                discordMessageId: message.id.toString(),
-                telegramMessageId: telegramMessage.message_id.toString(),
-                SubscriptionId: subscription.id,
-            });
-            if (subscription.retentionTime && retentionTime <= 1_000_000_000) {
-                setLongTimeout(() => {
-                    telegramBot.telegram.deleteMessage(telegramChannelId, telegramMessage.message_id);
-                }, 1000 * 60 * retentionTime);
-            }
+    let channel = await rabbitmq.createChannel();
+    channel.sendToQueue("task", Buffer.from(await appendSignature(format.escape(message.content))), {
+        headers: {
+            channelId: message.channelId
         }
-    }
+    });
 }
 
 async function appendSignature(content) {
--- telegram/index.mjs
+++ telegram/index.mjs
@@ -1,9 +1,12 @@
-import { Telegraf } from 'telegraf';
+import {Telegraf} from 'telegraf';
 import telegramEvents from './events/index.mjs';
 import session from '@telegraf/session';
 import stage from './scenes/index.mjs';
 import telegramCommands from './commands/index.mjs';
 import rabbitmq from "../rabbitmq/index.mjs";
+import db from "../db/index.mjs";
+import {setLongTimeout} from "long-settimeout";
+import {markdownv2 as format} from "telegram-format";
 
 let telegramBot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
 telegramBot.use(session());
@@ -17,14 +20,48 @@ telegramCommands.forEach((command, name) => {
 
 let channel = await rabbitmq.createChannel();
 await channel.assertQueue("task");
-await channel.consume("task", (msg) => {
-    console.log(msg);
-    if (msg !== null) {
-        console.log('Received:', msg.content.toString());
-        channel.ack(msg);
-    } else {
-        console.log('Consumer cancelled by server');
-    }
+await channel.consume("task", async (message) => {
+    await receiveMessage(message);
+    channel.ack(message);
 });
+
+async function receiveMessage(message) {
+    let subscriptions = await db.Subscription.findAll({
+        include: [
+            {
+                model: db.DiscordChannel,
+                attributes: ["channelId"]
+            },
+            {
+                model: db.TelegramChannel,
+                attributes: ["channelId"]
+            },
+        ]
+    })
+
+    let parsedSubscriptions = subscriptions.map(it => it.dataValues).map(it => ({
+        id: it.id,
+        discordChannelId: it.DiscordChannel.dataValues.channelId,
+        telegramChannelId: it.TelegramChannel.dataValues.channelId,
+        retentionTime: it.retentionTime
+    }));
+
+    for (let subscription of parsedSubscriptions) {
+        let {telegramChannelId, discordChannelId, retentionTime} = subscription;
+        if (message.properties.headers.channelId === discordChannelId) {
+            let telegramMessage = await telegramBot.telegram.sendMessage(telegramChannelId, format.escape(message.content.toString()), {parse_mode: 'MarkdownV2'});
+            await db.MessageLink.create({
+                discordMessageId: message.properties.headers.channelId,
+                telegramMessageId: telegramMessage.message_id.toString(),
+                SubscriptionId: subscription.id,
+            });
+            if (subscription.retentionTime && retentionTime <= 1_000_000_000) {
+                setLongTimeout(() => {
+                    telegramBot.telegram.deleteMessage(telegramChannelId, telegramMessage.message_id);
+                }, 1000 * 60 * retentionTime);
+            }
+        }
+    }
+}
 
 export default telegramBot;
